c1c8650a70f576e65f99e25b67ade81be9280363
Michael Scriven <bigscriv03@gmail.com>
2017-05-19 08:56:09 -0400

    Add partials and destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,45 +1,52 @@
 class ArticlesController < ApplicationController
 
-def new
-@article = Article.new
-end
-
-def edit
-  @article = Article.find(params[:id])
-end
-
-def index
-  @articles = Article.all
-end
-
-def create
-  @article = Article.new(article_params)
-  if @article.save
-    flash[:notice] = "Article was successfully created"
-    redirect_to article_path(@article)
-  else
-    render 'new'
+  def new
+  @article = Article.new
   end
-end
 
-def update
-  @article = Article.find(params[:id])
-  if @article.update(article_params)
-    flash[:notice] = "Article was successfully updated"
-    redirect_to article_path(@article)
-  else
-    render 'edit'
+  def edit
+    @article = Article.find(params[:id])
   end
-end
 
-def show
-  @article = Article.find(params[:id])
-end
-
-private
-  def article_params
-    params.require(:article).permit(:title, :description)
+  def index
+    @articles = Article.all
   end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was successfully deleted"
+    redirect_to articles_path
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
 
 
 end
